bdda2d744695a8e33dc311c561858c46947e9a9a
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-07-28 12:28:06 +0000

    add event listener to js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+/* add event listener */
+/** this listener will wait until the DOM is fully loaded */
+document.addEventListener("DOMContentLoaded", function() {
+    let button = document.getElementById("start-button");
+    button.addEventListener("click", rungame)
+})
+/* add rungame function*/
+
+function runGame {
+    
+}
+
+/* add score-calculation function */
