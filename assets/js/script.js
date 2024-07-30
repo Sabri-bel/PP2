8031d9b45021f72ad9f28b013017a8dac00338d9
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-07-30 19:04:43 +0000

    add javascript comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 
 /*retrieve information from the DOM */
 let button = document.getElementById("start-button");
-let playerScore = document.getElementById("user-score");
 let choices = ["heart-eyes", "horse", "worried_"];
 
 /* add event listener */
-/** this listener will wait until the DOM is fully loaded */
+
+/** this listener will wait until the DOM is fully loaded and activate another
+ * listener for the game button
+ */
 document.addEventListener("DOMContentLoaded", function() {
     button.addEventListener("click", runGame)
 })
 /* add rungame function*/
+
+/** this function will create a random number between 1 and 3 and use it 
+ * for generate the images. it will modify the alt information as well
+ */
 
 function runGame() {
     let randomImg1 = Math.floor(Math.random() * 3);
@@ -22,6 +28,14 @@ function runGame() {
     document.getElementById("card2").alt = choices[randomImg2];
     document.getElementById("card3").src = `assets/images/${choices[randomImg3]}.png`;
     document.getElementById("card3").alt = choices[randomImg3];
+    scoreCalculator()
 }
 
 /* add score-calculation function */
+
+/**this fucntion calculate the final score of the customer  */
+
+function scoreCalculator() {
+    let playScore = document.getElementById("user-score").innerText;
+    
+}
