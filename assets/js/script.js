0cbdd7bddb40d91a4a9b01f6efd4c5ff5e41f81f
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-07-30 09:00:50 +0000

    add rungame function for the images selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
 
+/*retrieve information from the DOM */
+let button = document.getElementById("start-button");
+let playerScore = document.getElementById("user-score");
+let choices = ["heart-eyes.png", "horse.png", "worried_.png"];
 
 /* add event listener */
 /** this listener will wait until the DOM is fully loaded */
 document.addEventListener("DOMContentLoaded", function() {
-    let button = document.getElementById("start-button");
-    button.addEventListener("click", rungame)
+    button.addEventListener("click", runGame)
 })
 /* add rungame function*/
 
-function runGame {
-    
+function runGame() {
+    let randomImg1 = Math.floor(Math.random() * 3);
+    let randomImg2 = Math.floor(Math.random() * 3);
+    let randomImg3 = Math.floor(Math.random() * 3);
+
+    document.getElementById("card1").src = choices[randomImg1];
+    document.getElementById("card2").src = choices[randomImg2];
+    document.getElementById("card3").src = choices[randomImg3];
 }
 
 /* add score-calculation function */
