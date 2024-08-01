53d3c605c3ed483e3079f3ff0cf72a706e4379a3
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-08-01 18:17:05 +0000

    add a function that reset the score if all images are different

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-/*retrieve information from the DOM */
+/*retrieve information from the DOM  and create constant*/
 const button = document.getElementById("start-button");
 const card1 = document.getElementById("card1");
 const card2 = document.getElementById("card2");
@@ -10,11 +10,14 @@ const choices = ["heart-eyes", "horse", "worried_"];
 /* add event listener */
 
 /** this listener will wait until the DOM is fully loaded and activate another
- * listener for the game button
+ * listener for activate the game button
  */
+
 document.addEventListener("DOMContentLoaded", function() {
     button.addEventListener("click", runGame)
 })
+
+
 /* add rungame function*/
 
 /** this function will create a random number between 1 and 3 and use it 
@@ -31,33 +34,51 @@ function runGame() {
     renderCard(card3, randomImg3);
 
     if (randomImg1 === randomImg2 && randomImg2 === randomImg3)  {
-        scoreCalculatorAll()
+        scoreCalculatorAll();
     }
     else if (randomImg1 === randomImg2 || randomImg1 === randomImg3) {
-        scoreCalculatorSome()
+        scoreCalculatorSome();
     }
     else if (randomImg2 === randomImg3) {
-        scoreCalculatorSome()
+        scoreCalculatorSome();
+    }
+    else {
+        scoreReset();
     }
 
 }
+
+/** this function modify the img src and alt structure based on the random images
+ * as a result of the previous function
+ */
 
 function renderCard(card, choiceIndex) {
     card.src = `${imageLocation}${choices[choiceIndex]}.png`;
     card.alt = choices[choiceIndex];
 }
+
 /* add score-calculation function */
 
-/**this fucntion calculate the final score of the customer  */
+/**this fucntion calculate and update the score of the user if the customer is
+ * getting all the 3 images 
+ */
 
 function scoreCalculatorAll() {
     let playScore = parseInt(document.getElementById("user-score").innerText);
    document.getElementById("user-score").innerText = playScore + 10;
 }
 
+/** this function will calculate and update the score of the user if the user is getting
+ * only 2 images out of 3
+ */
 function scoreCalculatorSome() {
     let playScore = parseInt(document.getElementById("user-score").innerText);
     document.getElementById("user-score").innerText = playScore + 5;
+}
+
+function scoreReset() {
+    let playScore = parseInt(document.getElementById("user-score").innerText);
+    document.getElementById("user-score").innerText = 0;
 }
 
 function showInstruction() {
