d49a3604b5e989d8c8e192422315a56c41ca21fe
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-08-05 12:02:19 +0000

    tidy up for duplicate code in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
 
 /*retrieve information from the DOM  and create constant*/
+
 const button = document.getElementById("start-button");
 const card1 = document.getElementById("card1");
 const card2 = document.getElementById("card2");
 const card3 = document.getElementById("card3");
+
+/*const used for assign the image as random image */
+
 const imageLocation = "assets/images/"
 const choices = ["heart-eyes", "horse", "worried_"];
 
+let userWin = 0
+let userLose = 0
+let playScore = parseInt(document.getElementById("user-score").innerText);
+
+
 /* add event listeners */
 
-/** this listener will wait until the DOM is fully loaded and activate another
- * first listener for activate the game button with click and second for enter key
+/* listener n.1 will wait until the DOM is fully loaded. It activate another
+   listener n.2 for the actual game button using the mouse click
  */
 
 document.addEventListener("DOMContentLoaded", function() {
     button.addEventListener("click", runGame)
 })
 
-/*use the enter key for staring the game instead of click*/
+/*event listener n3 use the enter key for starting the game instead of click*/
+
  document.getElementById("start-button").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             runGame();
@@ -25,10 +35,11 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 
 
+
 /* add rungame function*/
 
 /** this function will create a random number between 1 and 3 and use it 
- * for generate the images and score
+ * for generate the images
  */
 
 function runGame() {
@@ -42,27 +53,32 @@ function runGame() {
 
     if (randomImg1 === randomImg2 && randomImg2 === randomImg3)  {
         scoreCalculatorAll();
+        userWin += 1
     }
     else if (randomImg1 === randomImg2 || randomImg1 === randomImg3) {
         scoreCalculatorSome();
+        userWin += 1
     }
     else if (randomImg2 === randomImg3) {
         scoreCalculatorSome();
+        userWin += 1
     }
     else {
         scoreReset();
+        userLose += 1
     }
 
 }
 
-/** this function modify the img src and alt structure based on the random images
- * as a result of the previous function
- */
+/** this function modify the DOM img src and alt structure based on the 
+ * random images
+  */
 
 function renderCard(card, choiceIndex) {
     card.src = `${imageLocation}${choices[choiceIndex]}.png`;
     card.alt = choices[choiceIndex];
 }
+
 
 /* add score-calculation function */
 
@@ -71,7 +87,6 @@ function renderCard(card, choiceIndex) {
  */
 
 function scoreCalculatorAll() {
-    let playScore = parseInt(document.getElementById("user-score").innerText);
 
    document.getElementById("user-score").innerText = playScore + 10;
    if (playScore >= 30) {
@@ -85,7 +100,7 @@ function scoreCalculatorAll() {
  */
 
 function scoreCalculatorSome() {
-    let playScore = parseInt(document.getElementById("user-score").innerText);
+    /*let playScore = parseInt(document.getElementById("user-score").innerText);*/
 
     document.getElementById("user-score").innerText = playScore + 5;
     if (playScore >= 30) {
@@ -93,33 +108,43 @@ function scoreCalculatorSome() {
        }
 }
 
-/*reset the score when all images are different */
+/* this function reset the score when all images are different */
+
 function scoreReset() {
-    let playScore = parseInt(document.getElementById("user-score").innerText);
+    /*let playScore = parseInt(document.getElementById("user-score").innerText);*/
 
     document.getElementById("user-score").innerText = 0;
     showTryAgain()
 }
 
 
+/* functions below add some icons in the DOM based on the results */
+
 function showWins() {
-    // Create element:
 const para = document.createElement("span");
 para.setAttribute("id","win")
 para.innerHTML = `<i class="fa-solid fa-star"></i>`;
 
-// Append to another element:
+// Append to another existing element:
 document.getElementById("star").appendChild(para);
    
 }
 
+
 function showTryAgain() {
-        // Create element:
+
 const para = document.createElement("span");
 para.setAttribute("id", "lose")
 para.innerHTML = `<i class="fa-solid fa-skull"></i>`;
 
-// Append to another element:
+// Append to another existing DOM element:
 document.getElementById("star").appendChild(para);
-
 }
+
+function results() {
+    let result = document.createElement("div");
+    result.setAttribute("class", "final-results");
+    result.innerHTML = `Your final score is ${playScore}`;
+    document.getElementById("game-results").insertBefore(result)
+}
+
